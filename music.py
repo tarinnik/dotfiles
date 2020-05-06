f82970df55dac5160a0312e2085ef6e693a38470
Tinnik <tpkoster@protonmail.com>
2020-05-06 16:09:38 +1000

    rewritten in python

---
+++ music.py
@@ -0,0 +1,32 @@
+import sys
+import os
+import subprocess
+
+if len(sys.argv) > 1:
+    url = sys.argv[1]
+else:
+    print("No URL specified")
+    quit()
+
+if len(sys.argv) > 2:
+    start = sys.argv[2]
+else:
+    start = 0
+
+if len(sys.argv) > 3:
+    end = sys.argv[3]
+else:
+    end = None
+
+subprocess.call(["youtube-dl", "-f", "bestaudio", "-i", "-o", "~/tmpmusic/%(title)s.%(ext)s", url])
+
+os.chdir("/home/thomas/tmpmusic")
+tmpfile = os.listdir()[0]
+song_name = tmpfile[0:tmpfile.rfind('.')]
+
+if end == None:
+    subprocess.call(["ffmpeg","-ss", str(start), "-i", tmpfile, "/home/thomas/Music/" + song_name + ".mp3"])
+else:
+    subprocess.call(["ffmpeg","-ss", str(start), "-t", str(end), "-i", tmpfile, "/home/thomas/Music/" + song_name + ".mp3"])
+
+os.remove(tmpfile)
