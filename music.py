8c1ed04bd995434715f3f95e85a08771651ed6ed
Tinnik <tpkoster@protonmail.com>
2020-07-30 09:55:06 +1000

    added multiple file conversions

--- music.py
+++ music.py
@@ -5,8 +5,7 @@ import subprocess
 if len(sys.argv) > 1:
     url = sys.argv[1]
 else:
-    print("No URL specified")
-    quit()
+    url = None
 
 if len(sys.argv) > 2:
     start = sys.argv[2]
@@ -18,20 +17,18 @@ if len(sys.argv) > 3:
 else:
     end = None
 
-subprocess.call(["youtube-dl", "-f", "bestaudio", "-i", "-o", "~/tmpmusic/%(title)s.%(ext)s", url])
+if url is not None:
+    subprocess.call(["youtube-dl", "-f", "bestaudio", "-i", "-o", "~/tmpmusic/%(title)s.%(ext)s", url])
 
 os.chdir("/home/thomas/tmpmusic")
-try:
-    tmpfile = os.listdir()[0]
-except IndexError:
-    print("Unable to download the file")
-    quit()
+files = os.listdir()
 
-song_name = tmpfile[0:tmpfile.rfind('.')]
+for tmpfile in files:
+    song_name = tmpfile[0:tmpfile.rfind('.')]
 
-if end is None:
-    subprocess.call(["ffmpeg","-ss", str(start), "-i", tmpfile, "/home/thomas/Music/" + song_name + ".mp3"])
-else:
-    subprocess.call(["ffmpeg","-ss", str(start), "-t", str(end), "-i", tmpfile, "/home/thomas/Music/" + song_name + ".mp3"])
+    if end is None:
+        subprocess.call(["ffmpeg","-ss", str(start), "-i", tmpfile, "/home/thomas/Music/" + song_name + ".mp3"])
+    else:
+        subprocess.call(["ffmpeg","-ss", str(start), "-t", str(end), "-i", tmpfile, "/home/thomas/Music/" + song_name + ".mp3"])
 
-os.remove(tmpfile)
+    os.remove(tmpfile)
