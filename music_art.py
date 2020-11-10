6966dc31d31522d9a00f8bc5b4e32cd0d2c5e55a
Tinnik <tpkoster@protonmail.com>
2020-11-10 11:45:11 +1000

    added file conversion to jpg

--- music_art.py
+++ music_art.py
@@ -10,6 +10,12 @@ image = sys.argv[2]
 
 os.chdir(music)
 
+if image[image.rfind('.') + 1:] != "jpg":
+    image_name = image[:image.rfind('.')]
+    subprocess.call(["ffmpeg", "-i", image, f"{image_name}.jpg"])
+    os.remove(image)
+    image = f"{image_name}.jpg"
+
 subprocess.call(["ffmpeg", "-i", song, "-i", image, "-c", "copy", "-map", "0", "-map", "1", "-metadata:s:v", "title=\"Album cover\"", "-metadata:s:v", "comment=\"Cover (Front)\"", "temp.mp3"])
 os.remove(song)
 os.rename("temp.mp3", song)
