6347c46fc29edf7bd903f8aabbf7a2fa90ebd0c5
tk <tpk@tpk.net.au>
2024-02-24 12:10:58 +1100

    init nvim

---
+++ nvim/lua/config/keybinds.lua
@@ -0,0 +1,18 @@
+vim.keymap.set("n", "<leader>fv", vim.cmd.Ex)
+
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+
+-- Move lines up and down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Keep search terms in the middle
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Yank to system clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
